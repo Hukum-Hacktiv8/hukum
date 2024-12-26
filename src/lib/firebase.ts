70b314c359fe6770fc9b6d06a5d3a723db34fdf6
Kenny <kenn667914@gmail.com>
2024-12-26 19:06:19 +0700

    Create folder lib, finish firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+import { getFirestore } from "firebase/firestore";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUeiaH73ewG5th7Yg-fU5bhkRKfb5fICI",
+  authDomain: "hukum-rtc.firebaseapp.com",
+  projectId: "hukum-rtc",
+  storageBucket: "hukum-rtc.firebasestorage.app",
+  messagingSenderId: "409981818150",
+  appId: "1:409981818150:web:699c93a1f3eb8b31bf5de7",
+  measurementId: "G-XTJTW4YGLW",
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const db = getFirestore(app);
---
+++ src/lib/webrtc.ts
@@ -0,0 +1,4 @@
+// STUN Server
+export const configuration: RTCConfiguration = {
+  iceServers: [{ urls: "stun:stun1.l.google.com:19302" }, { urls: "stun:stun2.l.google.com:19302" }],
+};
