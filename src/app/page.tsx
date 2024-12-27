5ea797f5b2824d933d5432b7ca3fbc4a47da268f
Oka <okaaamhdt1099@gmail.com>
2024-12-27 19:59:46 +0700

    fix: merge auth chat & navbar footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,20 @@
-<<<<<<< HEAD
 import Carousel from "@/components/Carousel";
 import KonsultasiCard from "@/components/KonsultasiCard";
 import Navbar from "@/components/Navbar";
 
+import WaveDivider from "@/components/wave-divider";
+import Footer from "@/components/footer";
+
+
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Carousel />
-      <KonsultasiCard />
+            <Navbar />
+            <Carousel />
+            <KonsultasiCard />
+
+            <WaveDivider />
+            <Footer />
     </>
   );
 }
-=======
-import Home from "@/components/home/page";
-// import WaveDivider from "@/components/wave-divider";
-// import Footer from "@/components/footer";
-// import Navbar from "@/components/navbar";
-
-
-export default function Page() {
-    return (
-        <div>
-            <Home />
-            {/* <Navbar /> */}
-            {/* <WaveDivider /> */}
-            {/* <Footer /> */}
-        </div>
-    )
-}
->>>>>>> cecd9c34bd7da71c3296cc68361a38adddcbe2f1
