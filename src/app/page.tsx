946049c1373a4312d83a8cdc52efc75248e93499
Kenny <kenn667914@gmail.com>
2024-12-28 16:42:18 +0700

    Pull development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Carousel from "@/components/Carousel";
 import KonsultasiCard from "@/components/KonsultasiCard";
 import Navbar from "@/components/Navbar";
 import WaveDivider from "@/components/wave-divider";
-import Footer from "@/components/Footer";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
