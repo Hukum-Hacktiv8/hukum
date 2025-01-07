fc90e306a1b0b80161d69d25a1afa89b0ca8ba89
Ragaramadhan <ragaramadhannur2017@gmail.com>
2025-01-07 11:38:50 +0700

    feat:test

--- src/app/api/payment-intent/route.ts
+++ src/app/api/payment-intent/route.ts
@@ -22,9 +22,6 @@ export async function POST(request: NextRequest) {
   } catch (error) {
     console.error("Internal Error:", error);
 
-    return NextResponse.json(
-      { error: `Internal Server Error: ${error}` },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: `Internal Server Error: ${error}` }, { status: 500 });
   }
 }
